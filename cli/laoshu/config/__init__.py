9fd69401dcd196e8ca0d75fccefff299c993d01e
Bartosz Mikulski <mail@mikulskibartosz.name>
2025-06-14 15:29:20 +0800

    .dot env reader

---
+++ cli/laoshu/config/__init__.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from typing import Optional
+from .envs import read_env_vars
+
+
+@dataclass
+class Config:
+    scrapingant_api_key: Optional[str]
+
+
+def load_config() -> Config:
+    vars = read_env_vars()
+    return Config(scrapingant_api_key=vars.get("SCRAPINGANT_API_KEY"))
