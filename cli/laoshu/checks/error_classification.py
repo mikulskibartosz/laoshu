8203142a5f4ac5a58293499e2a79b69122c68735
Bartosz Mikulski <mail@mikulskibartosz.name>
2025-07-18 09:24:01 +0800

    Merge two faithfulness errors of the same type

--- cli/laoshu/checks/error_classification.py
+++ cli/laoshu/checks/error_classification.py
@@ -1,5 +1,5 @@
-from typing import List
-from laoshu.baml_client.types import FaithfulnessError
+from typing import Dict, List
+from laoshu.baml_client.types import FaithfulnessError, FaithfulnessErrorType
 from laoshu.baml_client import b
 
 
@@ -7,4 +7,13 @@ class ErrorClassificationCheck:
     async def classify_errors(
         self, claim: str, source_content: str
     ) -> List[FaithfulnessError]:
-        return await b.ClassifyFaithfulnessErrors(claim, source_content)
+        errors = await b.ClassifyFaithfulnessErrors(claim, source_content)
+
+        merged: Dict[FaithfulnessErrorType, FaithfulnessError] = {}
+        for error in errors:
+            key = error.error_type
+            if key in merged:
+                merged[key].reasoning += "\n\n" + error.reasoning
+            else:
+                merged[key] = error
+        return list(merged.values())
